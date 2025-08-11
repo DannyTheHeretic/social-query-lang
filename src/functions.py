2cbe07a732bb9d7d025db625e8f46fab10dfc012
Daniel <daniel@theheretic.work>
2025-08-10 21:18:17 -0500

    fix: Using the frontend.py update table in our example function

--- src/functions.py
+++ src/functions.py
@@ -4,38 +4,33 @@ from js import Event, document
 from pyodide.ffi import create_proxy
 from pyodide.http import pyfetch
 
-from frontend import CLEAR_BUTTON, EXECUTE_BUTTON, clear_interface
+from frontend import CLEAR_BUTTON, EXECUTE_BUTTON, clear_interface, update_table
 
 
-def parse_input(_: Event) -> None:
+async def parse_input(_: Event) -> None:
     """Start of the parser."""
     print("testing")
     y = document.getElementById("query-input").value.split("=")
     print(y[-1])  # TODO: Put SQL Parser in # tokenize(y) from parser import tokenize
-    return y
+    await get_user_data(y[-1])
 
 
 async def get_user_data(user: dict) -> dict:
     """Pyfetch command example."""
-    url = f"https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor={user[-2].text}"
+    url = f"https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor={user}"
     response = await pyfetch(url)
     val = (await response.json())["feed"]
     tb = document.getElementById("table-body")
     tb.innerHTML = ""
-
+    head = ["Name", "Post"]
+    body = []
     for i in val:
         data_point = i["post"]
         try:
-            tb.innerHTML += f"""<tr>
-<td colspan="2" style="text-align: center; padding: 20px; color: #666">
-            {data_point["author"]["displayName"]}
-</td>
-<td colspan="4" style="text-align: left; overflow-wrap: anywhere; word-wrap: break-word; padding: 20px; color: #666">
-            {data_point["record"]["text"].replace("\n", "<br>")}
-</td>
-</tr>"""
+            body.append([data_point["author"]["displayName"], data_point["record"]["text"]])
         except KeyError:
             continue  # Handle if a field is missing (no text maybe?)
+    update_table(head, body)
     return val
 
 
