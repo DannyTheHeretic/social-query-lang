f45db166549cd71ce88089d1657d911745db7857
Daniel <daniel@theheretic.work>
2025-08-11 01:21:26 -0500

    feat: added the extractors for the functions

--- src/functions.py
+++ src/functions.py
@@ -5,10 +5,13 @@ from pyodide.ffi import create_proxy
 from pyodide.http import pyfetch
 
 from frontend import CLEAR_BUTTON, EXECUTE_BUTTON, clear_interface, update_table
+from parser import ParentKind, TokenKind, Tree, parse, tokenize
 
 
-def flatten_response(data: dict) -> dict:
+def flatten_response(data: dict, query: list | None = None) -> dict:
     """Flatten a dictionary."""
+    if query is None:
+        query = []
     flattened_result = {}
 
     def _flatten(current: dict, name: str = "") -> dict:
@@ -20,36 +23,94 @@ def flatten_response(data: dict) -> dict:
             # for idx, i in enumerate(current):
             #     _flatten(i, name + str(idx) + "_")
             """
-        else:
+        elif query and name[:-1] in query:
             flattened_result[name[:-1]] = current  # Drops the extra _
+        else:
+            flattened_result[name[:-1]] = current
 
     _flatten(data)
     return flattened_result
 
 
+def extract_actor(tree: Tree) -> str | None:
+    """Extract the actor from the tree."""
+    if not tree.kind == ParentKind.FILE:
+        raise ValueError
+    stmt = tree.children[0]
+    for c in stmt.children:
+        if c.kind == ParentKind.WHERE_CLAUSE:
+            where_clause = c
+            break
+    else:
+        return None
+
+    expr = where_clause.children[1]
+
+    if expr.kind != ParentKind.EXPR_BINARY or expr.children[1].kind != TokenKind.EQUALS:
+        return None
+
+    if (
+        expr.children[0].children[0].kind == TokenKind.IDENTIFIER
+        and expr.children[0].children[0].text == "actor"
+        and expr.children[2].children[0].kind == TokenKind.STRING
+    ):
+        v = expr.children[2].children[0].text
+        return v[1:-1] if v[0] == "'" else v
+    if (
+        expr.children[2].children[0].kind == TokenKind.IDENTIFIER
+        and expr.children[2].children[0].text == "actor"
+        and expr.children[0].children[0].kind == TokenKind.STRING
+    ):
+        v = expr.children[0].children[0].text
+        return v[1:-1] if v[0] == "'" else v
+    return None
+
+
+def extract_fields(tree: Tree) -> str | None:
+    """Extract the actor from the tree."""
+    if not tree.kind == ParentKind.FILE:
+        raise ValueError
+    stmt = tree.children[0]
+    for c in stmt.children:
+        if c.kind == ParentKind.FIELD_LIST:
+            return c.children[::2]
+            break
+    else:
+        return None
+
+
 async def parse_input(_: Event) -> None:
     """Start of the parser."""
-    y = document.getElementById("query-input").value.split("=")
-    # TODO: Put SQL Parser in # tokenize(y) from parser import tokenize
-    await get_user_data(y[-1])
+    y = document.getElementById("query-input").value
+    tokens = parse(tokenize(y))
+    # TODO: Put SQL Parser in
+    await get_user_data(tokens)
 
 
-async def get_user_data(user: dict) -> dict:
+async def get_user_data(tokens: Tree) -> dict:
     """Pyfetch command example."""
+    user = extract_actor(tokens)
+    fields = extract_fields(tokens)
+    field_tokens = [i.children[0] for i in fields if i.kind != TokenKind.STAR]
+    print(field_tokens)
     url = f"https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor={user}"
     response = await pyfetch(url)
     val = (await response.json())["feed"]
     tb = document.getElementById("table-body")
     tb.innerHTML = ""
     head = []
+    if field_tokens:
+        head = [j.text for j in field_tokens]
     body = []
-
     for i in val:
         data = i
 
         d = flatten_response(data)
-        body.append(d)
-        [head.append(k) for k in d if k not in head]
+        if field_tokens:
+            body.append({j: d[j] for j in head})
+        else:
+            body.append(d)
+            [head.append(k) for k in d if k not in head]
 
     update_table(head, body)
     return val
