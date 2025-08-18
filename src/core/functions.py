0110c335c7e2f49e721beca6725ffd143eb98e89
daniel <daniel@theheretic.work>
2025-08-17 21:56:43 -0500

    feat: select * from tables

--- src/core/functions.py
+++ src/core/functions.py
@@ -221,6 +221,22 @@ async def processor(api: tuple[str, str], table: str) -> dict:  # noqa: C901, PL
             val = feed["followers"]
         else:
             pass
+    elif table == "tables":
+        val = [
+            {"Table_Name": _}
+            for _ in ["feed", "timeline", "profile", "suggestions", "likes", "followers", "following", "mutuals"]
+            for _ in [
+                "feed",
+                "timeline",
+                "profile",
+                "suggestions",
+                "suggested_feed",
+                "likes",
+                "followers",
+                "following",
+                "mutuals",
+            ]
+        ]
     return val
 
 
